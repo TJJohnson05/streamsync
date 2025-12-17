7c14776d887cc17d3994240525513cdf32c3db27
TJJohnson05 <tjj29@njit.edu>
2025-12-17 17:19:58 -0500

    Bug fixes with adding the quiz part 2

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import '../styles/Login.css';
 import { useNavigate } from 'react-router-dom';
 import { saveAuth } from '../utils/auth';
 
